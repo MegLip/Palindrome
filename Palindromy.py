6cfcc14bcf8b7b2b7c55c33a00e49d87bbc3e0b4
Magdalena Lipińska <szymczyk.meg@gmail.com>
2020-09-01 23:47:24 +0200

    Check palindrome

--- Palindromy.py
+++ Palindromy.py
@@ -1,6 +1,13 @@
 # Palindromy - Moduł 4.2
-def palindrome(string):   #commit1 Definicja funkcji 
-    if string == string[::-1]:  #commit2 Odwrócone litery
+def palindrome(string):      #commit1 Definicja funkcji 
+    if string == string[::-1]:   #commit2 Odwrócone litery
         return True
     else:
         return False
+string = "level"        #commit3 sprawdz palindrom
+wynik = palindrome(string)
+if wynik ==1:
+  print(True)
+else:
+  print(False)
+  
