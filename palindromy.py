b3d1a4fb11b6e5bc5c00fa9eb0b829c77ae59bbc
MegLip <70174916+MegLip@users.noreply.github.com>
2020-09-16 22:49:49 +0200

    Update and rename Palindromy.py to palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -1,4 +1,4 @@
-# Palindromy - Moduł 4.2
+# palindromy - Moduł 4.2
 def palindrome(string):      #commit1 Definicja funkcji 
     if string == string[::-1]:   #commit2 Odwrócone litery
         return True
