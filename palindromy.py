ae4bf1451d4e1f7945025e6ac167d2dd6956a3f4
MegLip <70174916+MegLip@users.noreply.github.com>
2020-09-17 23:49:30 +0200

    Update palindromy.py

--- palindromy.py
+++ palindromy.py
@@ -1,7 +1,9 @@
 # palindromy - Moduł 4.2
-def palindrome(string):      #commit1 Definicja funkcji 
-    if string == string[::-1]:   #commit2 Odwrócone litery
+def palindrome(string):       #commit1 Definicja funkcji 
+    if string == string[::-1]:    #commit2 Odwrócone litery
         return True
     else:
         return False
-print(palindrome("level"))        #commit3 sprawdz palindrom
+print(palindrome("level"))           #commit3 sprawdz palindrom
+print(palindrome("kobylamamalybok"))
+print(palindrome("nowytarggóryzakopanenapokazyróggratywon"))
