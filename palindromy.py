5d63422f6df3b46ced116f58c93b4a1b65bc8dfb
MegLip <70174916+MegLip@users.noreply.github.com>
2020-09-23 00:47:02 +0200

    Wersja No2 z metodą isalpha()

--- palindromy.py
+++ palindromy.py
@@ -5,22 +5,28 @@
 #    * Tworzymy zmienną, dla której chcemy sprawdzić czy nasz wyraz/zdanie jest palindromem.
 #    * Odwracamy wszystkie litery zmiennej i porównujemy z wersją nieodwróconą. Jeśli są sobie równe, dany wyraz/zdanie jest palindromem.
 #    * Funkcja jako wynik zwraca wartośc True lub False.
+#    * Korzystamy z metody str.replace(), str.lower(), str.isalpha().
 #    '''
 
-def palindrome(word):                                        #1 Definicja funkcji 
-    if word == word[::-1]:                                   #2 Odwrócone litery
-        return True
-    else:
-        return False
-print(palindrome("level"))                                   #3 Sprawdź palindrom
-print(palindrome("ananas"))
-
-
-def palindrome_s(sentence):                                  #4 Definicja funkcji
-  new = sentence.lower().replace(" ","").replace(",","")     #5 Małe litery, pomienięcie spacji i przecinków
-  if new[::1] == new[::-1]:                                  #6 Odwrócone litery
+#No 1
+def palindrome(sentence):                                    #1 Definicja funkcji
+  new = sentence.lower().replace(" ","").replace(",","")     #2 Małe litery, pomienięcie spacji i przecinków
+  if new == new[::-1]:                                       #3 Odwrócone litery
     return True
   else:
     return False
-print(palindrome_s("Kobyła ma mały bok,,,"))                 #7 Sprawdź palindrom
-print(palindrome_s("Ala ma kota"))
+print(palindrome("Kobyła ma mały bok,,,"))                   #4 Sprawdź palindrom
+print(palindrome("Ala ma kota"))
+
+#No 2
+def palindrome2(sentence2):                                 
+    new2 = [litera for litera in sentence2.lower() if litera.isalpha()]        #5 Wersja No2 z metodą isalpha()
+    print(new2)
+    return new2 == new2[::-1]
+    if new2 == new2[::-1]:
+        return True
+    else:
+        return False
+print(palindrome2("Rats live on no evil star"))
+print(palindrome2("Madam, I'm Adam"))
+print(palindrome2("Ada jodu dojada!!!"))
